2c5790a58f5240e64c879c959ed2350002303bf0
andy <hrc13@qq.com>
2020-05-21 16:20:56 +0800

    123

--- environment.py
+++ environment.py
@@ -126,7 +126,7 @@ class Environment():
         if V == 0:
             taking_rate = 0
         else:
-            taking_rate = 0.2 / V
+            taking_rate = 2 / V
         P = 1 - math.exp(-taking_rate * D * (1 - taking_rate * Su))
         random = np.random.rand()
         if (P >= random):
